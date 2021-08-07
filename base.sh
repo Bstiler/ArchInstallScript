851c76980dc21505a540ebaf54e684c3029b2c7f
Bruno Gregório <Bruno08new12@gmail.com>
2021-08-07 18:29:25 -0300

    Added network support out of the box

--- base.sh
+++ base.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR=`cd \`dirname $0\` && pwd`;
 source $SCRIPT_DIR/functions.sh;
 
-PACKAGES=(
+BASE_PACKAGES=(
     base
     linux-lts
     sudo
@@ -9,12 +9,30 @@ PACKAGES=(
     grub
     efibootmgr
     os-prober
+);
+
+NETWORK_PACKAGES=(
     dhcpcd
     wireless_tools
     wpa_supplicant
     netcf
     dialog
-)
+    networkmanager
+    networkmanager-vpnc
+    networkmanager-pptp
+    networkmanager-openconnect
+    network-manager-applet
+);
+
+SYSTEM_UTILS=(
+    xdg-utils
+);
+
+PACKAGES=(
+    ${BASE_PACKAGES[@]}
+    ${NETWORK_PACKAGES[@]}
+    ${SYSTEM_UTILS[@]}
+);
 
 # Install Base System
 STEP="Install Packades Step";
