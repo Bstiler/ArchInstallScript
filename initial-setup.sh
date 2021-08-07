b761fb03f09549d0137451c19c67d7e8f04bdb28
Bruno Gregório <reclusivebox@outlook.com>
2021-08-07 12:06:30 -0300

    Added a new step to update the mirrors

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,3 +1,6 @@
+SCRIPT_DIR=`cd \`dirname $0\` && pwd`;
+source $SCRIPT_DIR/functions.sh;
+
 # Check Netowrk
 stop_if_fail ping -c 3 1.1.1.1;
 
@@ -11,3 +14,10 @@ sed -i '#/en_US.UTF-8 UTF-8/c\en_US.UTF-8 UTF-8' /etc/locale.gen;
 sed -i '#/pt_BR.UTF-8 UTF-8/c\pt_BR.UTF-8 UTF-8' /etc/locale.gen;
 locale-gen;
 export LANG=pt_BR.UTF-8;
+
+# Fix mirrors and repos
+STEP="Syncing Mirrors";
+stop_if_fail reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist;
+
+STEP="Updating Repos";
+stop_if_fail pacman -Syy;
