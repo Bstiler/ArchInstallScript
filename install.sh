c205d330c145b5560a48ed7f5f3ec4c55c1abbb4
Bruno Gregório <Bruno08new12@gmail.com>
2021-08-07 18:38:15 -0300

    Re-organized code

--- install.sh
+++ install.sh
@@ -4,31 +4,31 @@ SCRIPT_DIR=`cd \`dirname $0\` && pwd`;
 
 # Step 1: Config some misc stuff
 echo "Initial Setup Started";
-source $SCRIPT_DIR/env-check.sh;
-source $SCRIPT_DIR/initial-setup.sh;
+source $SCRIPT_DIR/modules/modules/env-check.sh;
+source $SCRIPT_DIR/modules/initial-setup.sh;
 
 # Step 2: Setup Root filesystem
-source $SCRIPT_DIR/rootfs.sh;
+source $SCRIPT_DIR/modules/rootfs.sh;
 set_root_as_btrfs $ROOTFS;
 
 # Step 3: Mount partitions
-source $SCRIPT_DIR/mount.sh;
+source $SCRIPT_DIR/modules/mount.sh;
 mount_stuff;
 
 # Set up home folder
-source $SCRIPT_DIR/home.sh;
+source $SCRIPT_DIR/modules/home.sh;
 create_home;
 
 # Install Base System
-source $SCRIPT_DIR/base.sh;
+source $SCRIPT_DIR/modules/base.sh;
 
 # Generate FSTAB
-source $SCRIPT_DIR/fstab.sh;
+source $SCRIPT_DIR/modules/fstab.sh;
 generate_fstab;
 
 # Create Sub-script
 
-source $SCRIPT_DIR/chroot-script-generator.sh;
+source $SCRIPT_DIR/modules/chroot-script-generator.sh;
 export CHROOT_INTERNAL_PATH=/root/install.sh;
 
 generate_chroot_script;
