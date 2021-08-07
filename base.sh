94ee6e1e14386ce721e2e4fbd46e2102e29d7742
Bruno Gregório <reclusivebox@outlook.com>
2021-08-07 15:25:02 -0300

    Added array for packages

--- base.sh
+++ base.sh
@@ -1,6 +1,19 @@
 SCRIPT_DIR=`cd \`dirname $0\` && pwd`;
 source $SCRIPT_DIR/functions.sh;
 
+PACKAGES=(
+    base
+    sudo
+    zsh
+    grub
+    efibootmgr
+    dhcpcd
+    wireless_tools
+    wpa_supplicant
+    netcf
+    dialog
+)
+
 # Install Base System
 STEP="Install Packades Step";
-stop_if_fail pacstrap /mnt base sudo zsh grub efibootmgr dhcpcd;
+stop_if_fail pacstrap /mnt $PACKAGES[*];
