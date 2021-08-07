ac2151519e9b170940620193321b4e3088fc480d
Bruno Gregório <reclusivebox@outlook.com>
2021-08-07 12:07:14 -0300

    Removed pacman update tasks

--- base.sh
+++ base.sh
@@ -3,5 +3,4 @@ source $SCRIPT_DIR/functions.sh;
 
 # Install Base System
 STEP="Install Packades Step";
-stop_if_fail error_tolerant_pacman pacman -Sy;
-stop_if_fail error_tolerant_pacstrap pacstrap /mnt;
+stop_if_fail pacstrap /mnt;
