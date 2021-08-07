a4b99d7647b6f6ffe8c8f5314daefd177008875f
Bruno Gregório <reclusivebox@outlook.com>
2021-08-07 12:06:14 -0300

    Added Variable-based generation

--- chroot-script-generator.sh
+++ chroot-script-generator.sh
@@ -1,36 +1,44 @@
 # Create Sub-script
 
-touch /tmp/chroot-script.sh
+generate_chroot_script() {
+    if [ ! $1 ]
+    then
+        export CHROOT_SCRIPT_PATH=/tmp/chroot-script.sh;
+    else
+        export CHROOT_SCRIPT_PATH=$1;
+    fi
 
-# Base System Settings
+    touch $CHROOT_SCRIPT_PATH;
 
-echo "#\!/bin/sh" >> /tmp/chroot-script.sh;
-echo "" >> /tmp/chroot-script.sh;
-echo "sed -i '#/en_US.UTF-8 UTF-8/c\en_US.UTF-8 UTF-8' /etc/locale.gen;" >> /tmp/chroot-script.sh;
-echo "sed -i '#/pt_BR.UTF-8 UTF-8/c\pt_BR.UTF-8 UTF-8' /etc/locale.gen;" >> /tmp/chroot-script.sh;
-echo "locale-gen;" >> /tmp/chroot-script.sh;
-echo "echo LANG=pt_BR.UTF-8 ＞ /etc/locale.conf;" >> /tmp/chroot-script.sh;
-echo "export LANG=pt_BR.UTF-8;" >> /tmp/chroot-script.sh;
-echo "ln -s /usr/share/zoneinfo/America/Recife /etc/localtime;" >> /tmp/chroot-script.sh;
-echo "hwclock --systohc --utc;" >> /tmp/chroot-script.sh;
-echo "systemctl enable dhcpcd@eth0.service;" >> /tmp/chroot-script.sh;
-echo "pacman -Syu;" >> /tmp/chroot-script.sh;
-echo "pacman -S wireless_tools wpa_supplicant wpa_actiond netcf dialog sudo zsh grub efibootmgr;" >> /tmp/chroot-script.sh;
-echo "systemctl enable net-auto-wireless.service;" >> /tmp/chroot-script.sh;
-echo "touch /etc/vconsole.conf;" >> /tmp/chroot-script.sh;
-echo "echo 'KEYMAP=br-abnt2' >> /etc/vconsole.conf;" >> /tmp/chroot-script.sh;
-echo "echo 'FONT=Lat2-Terminus16' >> /etc/vconsole.conf;" >> /tmp/chroot-script.sh;
-echo "echo 'FONT_MAP=' >> /etc/vconsole.conf;" >> /tmp/chroot-script.sh;
-echo "echo 'Please define a password for the root user:';" >> /tmp/chroot-script.sh;
-echo "passwd;" >> /tmp/chroot-script.sh;
-echo "useradd -m -g users -G wheel,storage,power,sudo -s /usr/bin/zsh -d $FINAL_HOME $USERNAME;" >> /tmp/chroot-script.sh;
-echo "echo 'Please define a password for your user:';" >> /tmp/chroot-script.sh;
-echo "passwd $USERNAME;" >> /tmp/chroot-script.sh;
-echo "touch /etc/sudoers.d/wheel" >> /tmp/chroot-script.sh;
-echo "echo '%sudo ALL=(ALL) ALL' /etc/sudoers.d/wheel>> " >> /tmp/chroot-script.sh;
-echo "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch" >> /tmp/chroot-script.sh;
-echo "grub-mkconfig -o /boot/grub/grub.cfg" >> /tmp/chroot-script.sh;
-echo "" >> /tmp/chroot-script.sh;
-echo "" >> /tmp/chroot-script.sh;
+    echo "#\!/bin/sh" >> $CHROOT_SCRIPT_PATH;
+    echo "" >> $CHROOT_SCRIPT_PATH;
+    echo "sed -i '#/en_US.UTF-8 UTF-8/c\en_US.UTF-8 UTF-8' /etc/locale.gen;" >> $CHROOT_SCRIPT_PATH;
+    echo "sed -i '#/pt_BR.UTF-8 UTF-8/c\pt_BR.UTF-8 UTF-8' /etc/locale.gen;" >> $CHROOT_SCRIPT_PATH;
+    echo "locale-gen;" >> $CHROOT_SCRIPT_PATH;
+    echo "echo LANG=pt_BR.UTF-8 ＞ /etc/locale.conf;" >> $CHROOT_SCRIPT_PATH;
+    echo "export LANG=pt_BR.UTF-8;" >> $CHROOT_SCRIPT_PATH;
+    echo "ln -s /usr/share/zoneinfo/America/Recife /etc/localtime;" >> $CHROOT_SCRIPT_PATH;
+    echo "hwclock --systohc --utc;" >> $CHROOT_SCRIPT_PATH;
+    echo "systemctl enable dhcpcd@eth0.service;" >> $CHROOT_SCRIPT_PATH;
+    echo "systemctl enable net-auto-wireless.service;" >> $CHROOT_SCRIPT_PATH;
+    echo "pacman -Syu;" >> $CHROOT_SCRIPT_PATH;
+    echo "pacman -S wireless_tools wpa_supplicant wpa_actiond netcf dialog sudo zsh grub efibootmgr;" >> $CHROOT_SCRIPT_PATH;
+    echo "touch /etc/vconsole.conf;" >> $CHROOT_SCRIPT_PATH;
+    echo "echo 'KEYMAP=br-abnt2' >> /etc/vconsole.conf;" >> $CHROOT_SCRIPT_PATH;
+    echo "echo 'FONT=Lat2-Terminus16' >> /etc/vconsole.conf;" >> $CHROOT_SCRIPT_PATH;
+    echo "echo 'FONT_MAP=' >> /etc/vconsole.conf;" >> $CHROOT_SCRIPT_PATH;
+    echo "echo 'Please define a password for the root user:';" >> $CHROOT_SCRIPT_PATH;
+    echo "passwd;" >> $CHROOT_SCRIPT_PATH;
+    echo "useradd -m -g users -G wheel,storage,power -s /usr/bin/zsh -d $FINAL_HOME $USERNAME;" >> $CHROOT_SCRIPT_PATH;
+    echo "echo 'Please define a password for your user:';" >> $CHROOT_SCRIPT_PATH;
+    echo "passwd $USERNAME;" >> $CHROOT_SCRIPT_PATH;
+    echo "touch /etc/sudoers.d/wheel" >> $CHROOT_SCRIPT_PATH;
+    echo "echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers.d/wheel" >> $CHROOT_SCRIPT_PATH;
+    echo "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch" >> $CHROOT_SCRIPT_PATH;
+    echo "grub-mkconfig -o /boot/grub/grub.cfg" >> $CHROOT_SCRIPT_PATH;
+    echo "" >> $CHROOT_SCRIPT_PATH;
+    echo "" >> $CHROOT_SCRIPT_PATH;
 
-chmod +x /tmp/chroot-script.sh;
+    chmod +x $CHROOT_SCRIPT_PATH;
+}
+
